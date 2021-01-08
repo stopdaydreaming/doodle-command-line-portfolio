16afa759e4893fd79c52e49bb351c74acd8fd55b
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-08 11:15:40 -0500

    group practice

---
+++ homework-help.js
@@ -0,0 +1,48 @@
+//homework
+// intialize using this
+// set prompt user to a variable function
+// const promptUser = () => {
+//     inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "Enter your name",
+//       name: "userName"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter your location",
+//       name: "userLocation"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter your professional bio",
+//       name: "userBio"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter your linkedin",
+//       name: "userLinkedin"
+//     },
+//     {
+//       type: "input",
+//       message: "Enter your github",
+//       name: "userGithub"
+//     }
+//   ])
+//   .then(response =>
+//     fs.writeFile("portfolio.html", generateHTML(response), err =>
+//       err ? console.log(err) : console.log("it worked")
+//     )
+//   );
+// }
+// const init = () => {
+//     promptUser();
+// }
+// init();
+
+// module.exports = { generateHTML } //may not need curly braces
+// const generateHTML = require
+//add folder called utils
+//move generateMarkdown to utils
+//add file path for generateMarkdown to index.js
--- index.js
+++ index.js
@@ -1,8 +1,86 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const myHTML = `
+// <html>
+// <body>
+// <h1>hello world</h1>
+// </body>
+// </html>
+// `;
 
-const myHTML = `
-<html>
-<body>
-<h1>hello world</h1>
-</body>
-</html>
-`;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter your name",
+      name: "userName"
+    },
+    {
+      type: "input",
+      message: "Enter your location",
+      name: "userLocation"
+    },
+    {
+      type: "input",
+      message: "Enter your professional bio",
+      name: "userBio"
+    },
+    {
+      type: "input",
+      message: "Enter your linkedin",
+      name: "userLinkedin"
+    },
+    {
+      type: "input",
+      message: "Enter your github",
+      name: "userGithub"
+    }
+  ])
+  .then(response =>
+    fs.writeFile("portfolio.html", generateHTML(response), err =>
+      err ? console.log(err) : console.log("it worked")
+    )
+  );
+
+function generateHTML(response) {
+  return
+  `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>HTML Portfolio Page</title>
+        <!-- CSS only -->
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <h1 id="user-name">${response.userName}</h1>
+            <h3 id="user-location">${response.userLocation}</h3>
+            <h3>User Bio</h3>
+            <p id="user-bio">
+            ${response.userBio}
+            </p>
+            <button class="btn btn-primary btn-lg">
+                <a id="user-linkedin" href="${response.userLinkedin}" class="text-white">LinkedIn</a>
+                </button>
+            <button class="btn btn-primary btn-lg">
+                <a id="user-github" href="${response.userGithub}" class="text-white">Github</a>
+                </button>
+            </div>
+        </div>
+        </div>
+    </body>
+    </html>
+  `;
+}
+
